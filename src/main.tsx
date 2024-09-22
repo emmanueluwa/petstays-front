8abe3aab59bb292a7a72f8a8b5a7698137d4cb59
emmanuel <oemmanuel.uwadiae@gmail.com>
2024-09-22 11:57:53 +0100

    adding ability to register user from frontend

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,22 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <App />
+      </Router>
+    </QueryClientProvider>
   </StrictMode>
 );
