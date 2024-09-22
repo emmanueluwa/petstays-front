a481df4ea168ce429baadbc3029cea6b004a1e0f
emmanuel <oemmanuel.uwadiae@gmail.com>
2024-09-22 14:51:22 +0100

    adding token authentication to the react query app context prover

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import { useAppContext } from "../contexts/AppContext";
 
 const Header = () => {
+  const { isLoggedIn } = useAppContext();
   return (
     <div className="bg-teal-600 py-6">
       <div className="container mx-auto flex justify-between">
@@ -8,12 +10,20 @@ const Header = () => {
           <Link to="/">PeteineStays.com</Link>
         </span>
         <span className="flex space-x-2">
-          <Link
-            to="/sign-in"
-            className="flex items-center bg-white text-teal-400 px-3 font-bold hover:bg-teal-900"
-          >
-            Login
-          </Link>
+          {isLoggedIn ? (
+            <>
+              <Link to="/my-bookings">My Bookings</Link>
+              <Link to="/my-bookings">My Bookings</Link>
+              <button>Sign out</button>
+            </>
+          ) : (
+            <Link
+              to="/sign-in"
+              className="flex items-center bg-white text-teal-400 px-3 font-bold hover:bg-teal-900"
+            >
+              Login
+            </Link>
+          )}
         </span>
       </div>
     </div>
