407e2b8217133b3598c3ebf96b4b4eecfaf3e983
emmanuel <oemmanuel.uwadiae@gmail.com>
2024-09-26 15:31:04 +0100

    adding search parameters for filtering and sorting

--- src/api/api-client.ts
+++ src/api/api-client.ts
@@ -133,6 +133,16 @@ export const searchPlacesRequest = async (
   queryParams.append("childCount", searchParams.childCount || "");
   queryParams.append("page", searchParams.page || "");
 
+  queryParams.append("maxPrice", searchParams.maxPrice || "");
+  queryParams.append("sortOption", searchParams.sortOption || "");
+
+  searchParams.facilities?.forEach((facility) => {
+    queryParams.append("facilities", facility);
+  });
+
+  searchParams.types?.forEach((type) => queryParams.append("types", type));
+  searchParams.stars?.forEach((star) => queryParams.append("stars", star));
+
   const response = await fetch(
     `${API_BASE_URL}/api/places/search?${queryParams}`
   );
